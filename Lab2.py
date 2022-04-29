54ed86da98f7c34bdd02d9e8518a64b2bf60acc8
{kaffi} <{kfrkaffi.21@ichat.sp.edu.sg}>
2022-04-29 16:12:34 +0800

    Working check

--- Lab2.py
+++ Lab2.py
@@ -1,17 +1,23 @@
+def main():
+    display_main_menu()
+    x = get_user_input()
+    calc_average(x)
+    find_min_max(x)
+
 def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
 def get_user_input():
-    userInput = input()
-    splitInput = userInput.split(",")
-    floatList = float(splitInput)
-    return floatList
+    x = input()
+    y = x.split(",")
+    z = [float(i) for i in y]
+    return z
 
 def calc_average(tempList):
-    for number in tempList:
-        i = i + 1
-        total = total + number
-        average = total/i
-        return average
+    for x in range (0, len(tempList)):
+        total = total + tempList[x]
+    average = total/len(tempList)
+    return average
 
 def find_min_max(tempList):
     minMax = [int(min(tempList)), int(max(tempList))]
@@ -21,3 +27,6 @@ def sort_temperature():
     print("sort_temperature")
 def calc_median_temperature():
     print("calc_median_temperature")
+
+if __name__ == "__main__":
+    main()
